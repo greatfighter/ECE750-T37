41b1d3cd2b941d9995a8768c1e6b7b10cede5063
Jiaxin Ge <j3ge@uwaterloo.ca>
2024-09-23 19:53:13 -0400

    modify function and parameter in Monitor and Analyzer

--- A1/driver.py
+++ A1/driver.py
@@ -14,13 +14,13 @@ class Monitor:
 
         # Sysdig Data API Query Parameters
         # Pull the latest 5 minutes of data
-        self.START = -600
+        self.START = -300
         self.END = 0
         self.SAMPLING = 10
         self.FILTER = None #'kubernetes.namespace.name="acmeair-g4"'
     
     # Function to fetch data from IBM Cloud
-    def fetch_data_from_ibm_cloud(self, id, aggregation):
+    def fetch_data_from_ibm_monitor(self, id, aggregation):
         # Specify the metric to query
         metric = [
             # segmentation metric
@@ -61,18 +61,23 @@ class Monitor:
 class Analyzer:
     def __init__(self, metrics):
         # set relative weight for each property
-        self.weight_cpu = 0.2
-        self.weight_memory = 0.2
-        self.weight_latency = 0.45
-        self.weight_cost = 0.15
+        # Set relative weights for each metric
+        self.weight_cpu = 0.2      # CPU usage weight
+        self.weight_memory = 0.2   # Memory usage weight
+        self.weight_latency = 0.45 # Latency weight
+        self.weight_tps = 0.15     # Transactions per second (TPS) weight
+        self.weight_gc_time = 0.15 # GC time weight
+        
+        # Add weight for cost (CPU, memory, pod cost)
+        self.weight_cost = 0.1     # Cost weight (initialized)
         self.metrics = metrics
         self.services = ['acmeair-bookingservice', 'acmeair-customerservice', 'acmeair-authservice',
                          'acmeair-flightservice', 'acmeair-mainservice']
 
-    def triggerAdaptation(self, cpu, memory, latency):
+    def triggerAdaptation(self, cpu, memory, latency, tps, gc_time):
         # Check whether adaptation is needed based on CPU, memory, and latency values
-        print(f"CPU: {cpu:.2f}, memory: {memory:.2f}, latency: {latency:.2f}")
-        if cpu > 50 or memory > 50 or latency > 2e7:
+        print(f"CPU: {cpu:.2f}%, Memory: {memory:.2f}%, Latency: {latency:.2f}ms, TPS: {tps:.2f}, GC Time: {gc_time:.2f}ms")
+        if cpu > 80 or memory > 80 or latency > 1000 or tps < 50 or gc_time > 500:
             return True
         else:
             return False
@@ -104,6 +109,24 @@ class Analyzer:
         else:
             return 0
     
+    def utility_preference_tps(self, tps):
+        # Determine the utility preference for TPS
+        if tps > 100:
+            return 1
+        elif tps > 50:
+            return 0.5
+        else:
+            return 0
+
+    def utility_preference_gc_time(self, gc_time):
+        # Determine the utility preference for GC
+        if gc_time < 200:
+            return 1
+        elif gc_time < 500:
+            return 0.5
+        else:
+            return 0
+
     def utility_preference_cost(self, cost):
         # Determine the utility preference for cost (cpu, memory, pod)
         if cost == "cpu" or cost == "memory":
@@ -111,13 +134,15 @@ class Analyzer:
         else:
             return 0.5
 
-    def calculate_utility(self, cpu, memory, latency, cost):
+    def calculate_utility(self, cpu, memory, latency, tps, gc_time, cost):
         # Calculate the overall utility score based on weighted preferences
         cpu_utility = self.weight_cpu * self.utility_preference_cpu(cpu)
         memory_utility = self.weight_memory * self.utility_preference_memory(memory)
         latency_utility = self.weight_latency * self.utility_preference_latency(latency)
+        tps_utility = self.weight_tps * self.utility_preference_tps(tps)
+        gc_time_utility = self.weight_gc_time * self.utility_preference_gc_time(gc_time)
         cost_utility = self.weight_cost * self.utility_preference_cost(cost)
-        return cpu_utility + memory_utility + latency_utility + cost_utility
+        return cpu_utility + memory_utility + latency_utility + tps_utility + gc_time_utility + cost_utility
 
     def create_dataframe(self, filename):
         # Create a DataFrame from a JSON file
@@ -136,7 +161,7 @@ class Analyzer:
         # perform data preprocessing and check whether adaptation is required
         outputs = [[0]*len(self.metrics) for i in range(5)]
         for idx, (metric_id, aggregation) in enumerate(self.metrics):
-            filename = "datasets/" + metric_id + "_" + aggregation + "_metric.json"
+            filename = "datasets/" + metric_id.replace('.', '_') + "_" + aggregation + "_metric.json"
             df = self.create_dataframe(filename)
             aggregationData = df.groupby('service')
             avg_values = aggregationData['value'].mean()
@@ -148,7 +173,9 @@ class Analyzer:
             cpu = outputs[i][0]
             memory = outputs[i][1]
             latency = outputs[i][2]
-            adaptation = self.triggerAdaptation(cpu, memory, latency)
+            tps = outputs[i][3]
+            gc_time = outputs[i][4]
+            adaptation = self.triggerAdaptation(cpu, memory, latency, tps, gc_time)
             if adaptation:
                 print(f"{self.services[i]} requires adaptation")
                 self.find_best_strategy(outputs[i])
@@ -156,23 +183,40 @@ class Analyzer:
                 print(f"No adaptation required for {self.services[i]}")
 
     def find_best_strategy(self, output):
-        # Find the best adaptation strategy based on utility functions
         cpu = output[0]
         memory = output[1]
         latency = output[2]
-        # strategy1: increase cpu
-        utility1 = self.calculate_utility(cpu / 2.0, memory, latency / 2.0, "cpu")
-        # strategy2: increase memory
-        utility2 = self.calculate_utility(cpu, memory / 2.0, latency / 2.0, "memory")
-        # strategy3: increase pod
-        utility3 = self.calculate_utility(cpu / 2.0, memory / 2.0, latency / 4.0, "pod")
+        tps = output[3]
+        gc_time = output[4]
+        
+        # Strategy 1: Increase CPU resources (assume increasing CPU reduces CPU usage and latency)
+        # Increasing CPU lowers CPU usage and latency, slightly improves TPS, GC time remains unchanged
+        utility1 = self.calculate_utility(cpu / 1.5, memory, latency / 1.2, tps * 1.1, gc_time, "cpu")
+        
+        # Strategy 2: Increase memory resources (assume increasing memory reduces GC time and latency)
+        # Increasing memory reduces memory usage and GC time, slightly improves TPS, and lowers latency
+        utility2 = self.calculate_utility(cpu, memory / 2.0, latency / 1.1, tps * 1.05, gc_time / 1.5, "memory")
+        
+        # Strategy 3: Increase the number of Pods (assume more Pods improve TPS and reduce latency)
+        # Adding more Pods significantly improves TPS and reduces latency, with minor impact on CPU and memory usage
+        utility3 = self.calculate_utility(cpu / 1.2, memory / 1.2, latency / 1.5, tps * 1.5, gc_time / 1.1, "pod")
+        
+        # Strategy 4: Optimize garbage collection (assume optimizing GC time improves overall performance)
+        # Reducing GC time improves performance by optimizing JVM or allocating more memory
+        utility4 = self.calculate_utility(cpu, memory, latency / 1.05, tps, gc_time / 2.0, "gc")
+        
+        # Calculate utilities for each strategy and select the best one
+        utilities = {
+            'Increase CPU': utility1,
+            'Increase Memory': utility2,
+            'Increase Pod': utility3,
+            'Optimize GC': utility4
+        }
+        
+        # Select the strategy with the highest utility value
+        best_strategy = max(utilities, key=utilities.get)
+        print(f"Best strategy: {best_strategy}")
 
-        if utility1 > utility2 and utility1 > utility3:
-            print("Best strategy: Increase cpu")
-        elif utility2 > utility1 and utility2 > utility3:
-            print("Best strategy: Increase memory")
-        else:
-            print("Best strategy: Increase pod")
 
 def main():
     # IBM Cloud API Credentials
@@ -181,36 +225,29 @@ def main():
     GUID = "3fed93bc-00f4-4651-8ce2-e73ba4b9a918"
 
     # metrices: time aggregation is average
-    avg_metric_ids = ["cpu.quota.used.percent",
-                      "cpu.used.percent",
-                      "memory.limit.used.percent",
-                      "memory.bytes.used",
-                      "mongodb.connections.current",
-                      "jvm.class.loaded",
-                      "jvm.class.unloaded",
-                      "jvm.gc.global.time",
-                      "jvm.gc.global.count",
-                      "jvm.thread.count",
-                      "jvm.heap.used",
-                      "jvm.nonHeap.used"
-                      ]
+    avg_metric_ids = [
+        "cpu.used.percent",          
+        "memory.used.percent",       
+        "jvm.gc.global.time"         
+    ]
 
     # metrices: time aggregation is maximum
     max_metric_ids = [
-        "net.http.request.time"
+        "net.http.request.time" 
     ]
 
     # metrices: time aggregation is summation
     sum_metric_ids = [
-        "net.connection.count.out",
         "net.request.count.in"
     ]
 
     # metrices: core metrics used to perform adaptation analysis
     core_metrics = [
-        ("cpu.quota.used.percent", "avg"),
-        ("memory_limit_used_percent", "avg"),
-        ("net_http_request_time", "max")
+        ("cpu.used.percent", "avg"),
+        ("memory.used.percent", "avg"),
+        ("net.http.request.time", "max"),
+        ("net.request.count.in", "sum"),
+        ("jvm.gc.global.time", "avg")
     ]
 
     monitor = Monitor(URL, APIKEY, GUID)
@@ -219,13 +256,13 @@ def main():
     while True:
         # Fetch data from IBM Cloud
         for id in avg_metric_ids:
-            monitor.fetch_data_from_ibm_cloud(id, "avg")
+            monitor.fetch_data_from_ibm_monitor(id, "avg")
 
         for id in sum_metric_ids:
-            monitor.fetch_data_from_ibm_cloud(id, "sum")
+            monitor.fetch_data_from_ibm_monitor(id, "sum")
 
         for id in max_metric_ids:
-            monitor.fetch_data_from_ibm_cloud(id, "max")
+            monitor.fetch_data_from_ibm_monitor(id, "max")
         print(f"Pulling metrics from IBM Cloud")
         analyzer.process_data()
         # Sleep for 5 minutes (300 seconds)
