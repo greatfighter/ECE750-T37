177412ed87ee34335107cf49d7db0ff6469e094f
Jiaxin Ge <j3ge@uwaterloo.ca>
2024-11-05 15:26:31 -0500

    replace namespace to fit IBM group 4 env

--- project/k8s/app-setup.sh
+++ project/k8s/app-setup.sh
@@ -2,9 +2,9 @@ echo "Setting up namespace"
 
 kubectl apply -f namespace.yaml
 
-echo "Applying the namespace to the service mesh member roll"
+# echo "Applying the namespace to the service mesh member roll"
 
-kubectl replace -f service-member-roll.yaml
+# kubectl replace -f service-member-roll.yaml
 
 echo "Setting up recommendations"
 
@@ -20,4 +20,4 @@ kubectl apply -f orders.yaml
 
 echo "The non-service mesh route to the API is in the following output:"
 
-kubectl get routes -n service-mesh-demo
+kubectl get routes -n group-4
